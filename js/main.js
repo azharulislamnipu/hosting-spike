9fb579206a6851dde201c58d4977903e9aca4dfb
azhar <azharulislamnipu@gmail.com>
2019-10-05 11:58:26 +0600

    issue updated

--- js/main.js
+++ js/main.js
@@ -9,25 +9,13 @@
     -----------------------------------*/
     $win.scroll(function() {
         if ($(this).scrollTop() > 130){
-            $('header').addClass("sticky");
+            $('.header').addClass("sticky");
         }
         else{
-            $('header').removeClass("sticky");
+            $('.header').removeClass("sticky");
         }
     });
 
-    /*--
-        isotope
-        -----------------------------------*/
-    if ($('.breed-list').length>0){
-        var $grid = $('.breed-list').isotope({
-            itemSelector: '.breed-item',
-        });
-        $('.search-filter').on( 'click', 'li>a', function() {
-            var filterValue = $(this).attr('data-filter');
-            $grid.isotope({ filter: filterValue });
-        });
-    }
 
     
 })(jQuery);
